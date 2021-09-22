90bc8f02201aa2d17102b5437503e09a5b507603
Oskar Bjoerkqvist <objoerkqvist@eeibt032.d.ethz.ch>
2021-09-22 16:34:57 +0200

    andi peripheral

--- PERIPHERAL_bluetooth_retreat/BLETrackPeripheral.ino
+++ PERIPHERAL_bluetooth_retreat/BLETrackPeripheral.ino
@@ -1,14 +1,27 @@
+/*
+  BLE_Peripheral.ino
+
+  This program uses the ArduinoBLE library to set-up an Arduino Nano 33 BLE 
+  as a peripheral device and specifies a service and a characteristic. Depending 
+  of the value of the specified characteristic, an on-board LED gets on. 
+
+  The circuit:
+  - Arduino Nano 33 BLE. 
+
+  This example code is in the public domain.
+*/
+
 #include <ArduinoBLE.h>
       
 
 const char* deviceServiceUuid = "19b10000-e8f2-537e-4f6c-d104768a1214";
 const char* deviceServiceCharacteristicUuid = "19b10001-e8f2-537e-4f6c-d104768a1214";
 
+int something = -1;
+byte i = 0;
 
 BLEService someService(deviceServiceUuid); 
 BLEByteCharacteristic someCharacteristic(deviceServiceCharacteristicUuid, BLERead | BLEWrite);
-
-int something = -1;
 
 
 void setup() {
@@ -19,12 +32,11 @@ void setup() {
     Serial.println("- Starting BLE module failed!");
     while (1);
   }
-
   BLE.setLocalName("Arduino Nano 33 BLE (Peripheral)");
   BLE.setAdvertisedService(someService);
   someService.addCharacteristic(someCharacteristic);
   BLE.addService(someService);
-  someCharacteristic.writeValue(-1);
+  someCharacteristic.writeValue((byte) 1);
   BLE.advertise();
 
   Serial.println("Nano 33 BLE (Peripheral Device)");
@@ -41,15 +53,28 @@ void loop() {
     Serial.print("* Device MAC address: ");
     Serial.println(central.address());
     Serial.println(" ");
+    Serial.println(someCharacteristic.value());
+    someCharacteristic.writeValue((byte) 2);
+    Serial.println(someCharacteristic.value());
 
     while (central.connected()) {
+//      writeRegister();
+      //i++;
+      //Serial.println(someCharacteristic.value());
+      //someCharacteristic.writeValue((byte) i);
       if (someCharacteristic.written()) {
          something = someCharacteristic.value();
          Serial.println(something);
+         someCharacteristic.writeValue((byte) something+10);
        }
+       //delay(1000);
     }
     
     Serial.println("* Disconnected to central device!");
   }
-  
 }
+
+//void writeRegister(){
+//    i++;
+//    someCharacteristic.writeValue(i);
+//}
